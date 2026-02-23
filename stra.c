ca5db2ae08c6ee4800a2d66e789394d9f65190cd
David Blankson-Hemans <djbh@dynamic-oit-ip4-wifirestricted03-10-49-217-95.princeton.edu>
2026-02-23 03:59:37 -0500

    Parse Error Fix

--- stra.c
+++ stra.c
@@ -1,24 +1,31 @@
-
-#include <stdio.h>
+#include <stddef.h>
 #include <assert.h>
 
 size_t Str_getLength(const char pcSrc[])
 {
    size_t uLength = 0;
+
    assert(pcSrc != NULL);
+
    while (pcSrc[uLength] != '\0')
       uLength++;
+
    return uLength;
 }
 
 char *Str_copy(char pcDest[], const char pcSrc[])
 {
    size_t uLength;
+   size_t i;
+
    assert(pcSrc != NULL);
    assert(pcDest != NULL);
+
    uLength = Str_getLength(pcSrc);
-   for (size_t i = 0; i < uLength; i++)
+
+   for (i = 0; i < uLength; i++)
       pcDest[i] = pcSrc[i];
+
    pcDest[uLength] = '\0';
    return pcDest;
 }
@@ -28,6 +35,7 @@ char *Str_concat(char pcDest[], const char pcSrc[])
    size_t vLength;
    size_t wLength;
    size_t uLength;
+   size_t i;
 
    assert(pcSrc != NULL);
    assert(pcDest != NULL);
@@ -36,7 +44,7 @@ char *Str_concat(char pcDest[], const char pcSrc[])
    wLength = Str_getLength(pcSrc);
    uLength = vLength + wLength;
 
-   for (size_t i = 0; i < wLength; i++)
+   for (i = 0; i < wLength; i++)
       pcDest[vLength + i] = pcSrc[i];
 
    pcDest[uLength] = '\0';
@@ -56,6 +64,7 @@ int Str_compare(const char pcS1[], const char pcS2[])
          return -1;
       else if (pcS1[i] > pcS2[i])
          return 1;
+
       i++;
    }
 
@@ -75,12 +84,15 @@ char *Str_search(const char pcHaystack[], const char pcNeedle[])
    assert(pcHaystack != NULL);
    assert(pcNeedle != NULL);
 
+   /* Empty needle matches at start (like strstr). */
    if (pcNeedle[0] == '\0')
       return (char *)pcHaystack;
 
    for (i = 0; pcHaystack[i] != '\0'; i++)
    {
-      for (j = 0; pcNeedle[j] != '\0' && pcHaystack[i + j] != '\0'; j++)
+      for (j = 0;
+           pcNeedle[j] != '\0' && pcHaystack[i + j] != '\0';
+           j++)
       {
          if (pcHaystack[i + j] != pcNeedle[j])
             break;
